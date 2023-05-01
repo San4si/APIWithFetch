16bb494c8b6994b9b67012ae016e4c9d6a5d7a0d
Sandro F Brito <san_juq@hotmail.com>
2023-05-01 00:19:37 -0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const characterId  = document.getElementById('characterId');
+const btnGo = document.getElementById('btn-go');
+const content = document.getElementById('content');
+const image = document.getElementById('img');
+
+const fetchAPI =(value) =>{
+    const result = fetch(`https://rickandmortyapi.com/api/character/${value}`).then((res)=> res.json())
+    .then((data) => {
+        console.log(data);
+        return data;
+    });
+
+    return result;
+}
+
+
+
+btnGo.addEventListener('click', async (event) => {
+    event.preventDefault();
+const result = await fetchAPI(characterId.value);
+    content.textContent = `${JSON.stringify(result, undefined, 2)}`
+    image.src = `${result.image}`
+})
